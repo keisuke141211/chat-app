bfd58c271d320e2ff2e5b5d540ac6fdd12d2f126
keisuke141211 <keisuke141211tada@gmail.com>
2021-03-22 16:37:47 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "messages#index"
   resources :users, only: [:edit,:update,:destory]
+  resources :rooms, only: [:new,:create]
+
 end
